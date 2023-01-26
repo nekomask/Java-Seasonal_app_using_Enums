fc8d6dcad6ba8b270a2e0396faa005f00a15c3cf
kittenmasque <volaju@gmail.com>
2023-01-25 18:20:34 -0800

    refactored switch cases one more time to simplify our whichSeason method using our Enums class

--- src/Main.java
+++ src/Main.java
@@ -12,27 +12,11 @@ public class Main {
     }
 
     public static void whichSeason(Month month){
-        switch (month){
-            case DECEMBER:
-            case JANUARY:
-            case FEBRUARY:
-                System.out.printf("%s is in the Winter season. \n", month );
-                break;
-            case MARCH:
-            case APRIL:
-            case MAY:
-                System.out.printf("%s is in the Spring season. \n", month );
-                break;
-            case JUNE:
-            case JULY:
-            case AUGUST:
-                System.out.printf("%s is in the Summer season. \n", month );
-                break;
-            case SEPTEMBER:
-            case OCTOBER:
-            case NOVEMBER:
-                System.out.printf("%s is in the Autumn season. \n", month );
-                break;
+        switch (month) {
+            case DECEMBER, JANUARY, FEBRUARY -> System.out.printf("%s is in the Winter season. \n", month);
+            case MARCH, APRIL, MAY -> System.out.printf("%s is in the Spring season. \n", month);
+            case JUNE, JULY, AUGUST -> System.out.printf("%s is in the Summer season. \n", month);
+            case SEPTEMBER, OCTOBER, NOVEMBER -> System.out.printf("%s is in the Autumn season. \n", month);
         }
     }
     }
